b9c92308141b44df98fb02869cf9bc13af9d7ddb
Linsmar <lolvegeta7@gmail.com>
2019-10-28 19:44:18 +0100

    Primeira questao feita

--- q1.cpp
+++ q1.cpp
@@ -1,5 +1,28 @@
 #include <iostream>
 using namespace std;
+struct cell {
+    int element;
+    struct cell *next;
+};
+typedef struct cell celula;
+//Função de criação de uma célula da lista
+celula *newCell (){
+    celula *p = (celula *)malloc(sizeof(celula));
+    if (p) {
+        p->element = -1;
+        p->next = NULL;
+    }
+    return p;
+}
+//Função de inserir células no final da lista
+void insert (int e, celula *l) {
+    celula *p = newCell();
+    if (p) {
+        p->element = e;
+        while (l->next) l = l->next;
+        l->next = p;
+    }
+}
 
 int main (int argc, char *argv[]) {
     //IF caso o usuário não coloque a quantidade correta de argumentos
@@ -8,61 +31,82 @@ int main (int argc, char *argv[]) {
         cout << "Exemplo: " << argv[0] << ".exe in.txt out.txt" << endl;
         return 1;
     }
+
     //Declaração
     FILE *in;
     FILE *out;
-    int lista1[5];
-    int lista2[5];
-    int aux = 0;
-    char col;
+    int aux = 0, enableaux = 0; //Variavel enableaux serve pra tirar as duplicatas na 2° lista
+    int qntdLista1 = 0, qntdLista2 = 0; //Essas variaveis servem pra contar quantos elementos tem na lista,
+    char col;                           //podia ser feito também dentro da própria struct da lista.
+    celula *caux = newCell(); // Celula auxiliar
+    celula *h1 = newCell(); //1º Lista
+    celula *h2 = newCell(); //2º Lista
+
     //Abrindo o arquivo de entrada  
     in = fopen(argv[1], "r");
     if (in == NULL) cout << "O arquivo não foi aberto" << endl;
     else cout << "O arquivo foi aberto" << endl;
-    //Passando o que tem dentro do arquivo pro 1º vetor
-    for (int i = 0; i < 5; i++) {
+
+    //Passando o que tem dentro do arquivo para a 1ª lista
+    for (qntdLista1 = 0; col != ']'; qntdLista1++) {
+        fscanf (in, "%d", &aux);
+        fscanf (in, "%c", &col); //Essa variável serve pra procurar o caracter '['
+        if (col != '[') {
+            insert(aux, h1);
+        }
+        else if (col == ']') break;
+    }
+    fscanf (in, "%d", &aux);
+    fscanf (in, "%c", &col);
+    //Passando o que tem dentro do arquivo para a 2ª lista RETIRANDO AS DUPLICATAS
+    for (qntdLista2 = 0; col != ']' ; qntdLista2++) {
         fscanf (in, "%d", &aux);
         fscanf (in, "%c", &col);
         if (col != '[') {
-            lista1[i] = aux;
+            caux = h1->next;
+            for (int i = 0; i < qntdLista1-1; i++) { //Loop para tirar as duplicatas na 2° lista
+                if (caux->element == aux) {
+                    enableaux = 0; //Se essa variavel for igual a 0, significa que o número na lista é igual a outro número na lista 1.
+                    qntdLista2--;  //Como é igual, não vai ser inserido na 2° lista, portanto, diminuir o tamanho dela.
+                    break;
+                }
+                else enableaux = 1;
+                caux = caux->next;
+            }
+            if (enableaux == 1) { //Se for igual a 1, não tem duplicata, pode inserir na lista 2.
+                insert(aux, h2);
+                enableaux = 0;
+            }
         }
-        else if (col == '[') i = -1;
         else if (col == ']') break;
     }
-    //Passando o que tem dentro do arquivo pro 2º vetor
-    for (int i = 0; i < 5; i++) {
-        fscanf (in, "%d", &aux);
-        fscanf (in, "%c", &col);
-        if (col != '[') {
-            lista2[i] = aux;
-        }
-        else if (col == '[') i = -1;
-        else if (col == ']') break;
-    }
-    fclose(in);
-    for (int i = 0; i < 5; i++) {
-        cout << "Lista1: " << lista1[i] << " " << " Lista2: " << lista2[i] << " ";
-    }
-    cout << endl;
+    fclose(in); //Fechando o arquivo
+
     //Abrindo arquivo de saida
     out = fopen(argv[2], "w");
     if (!(out = fopen(argv[2], "w"))) cout << "O arquivo não foi aberto" << endl;
     else cout << "O arquivo foi aberto" << endl;
-    //Escrevendo no arquivo
-    fprintf(out, "[");
-    for (int i = 0; i < 5; i++) {
-        fprintf(out, "%d", lista1[i]);
-        if (i < 4) fprintf(out, " ");
-    }
-    fprintf(out, "]");
-    fprintf(out, " ");
-    fprintf(out, "[");
-    for (int i = 0; i < 5; i++) {
-        fprintf(out, "%d", lista2[i]);
-        if (i < 4) fprintf(out, " ");
-    }
-    fprintf(out, "]");
-    fclose(out);
 
-return 0;
+    //Passando a 1º lista inteira pra saida
+    fprintf(out, "[");
+    if (h1->next) {
+        caux = h1->next;
+        for (int i = 0; i < qntdLista1-1; i++) {
+            fprintf(out, "%d", caux->element);
+            if (i < qntdLista1-2) fprintf(out, " ");
+            caux = caux->next;
+        }
+    }
+    //Passando a 2º lista, sem duplicatas pra saida
+    if (h2->next) {
+        caux = h2->next;
+        fprintf(out, " ");
+        for (int i = 0; i < qntdLista2; i++) {
+            fprintf(out, "%d", caux->element);
+            if (i < qntdLista2-1) fprintf(out, " ");
+            caux = caux->next;
+        }
+    }
+    fprintf(out, "]");
+    return 0;
 }
